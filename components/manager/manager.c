b91aefa71d906cf50c64c0809c69ee17f5a60a97
Arkadiusz Czerwinski <A.Czerwinski@pistacje.net>
2025-01-31 20:54:23 -0600

    Add manager thread

--- components/manager/manager.c
+++ components/manager/manager.c
@@ -14,16 +14,28 @@
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 
+#include "msg.h"
 #include "manager.h"
 #include "mgr_reg.h"
 
 #include "mgr_reg_list.h"
 
 
+#define MGR_MSG_MAX           20
+
+#define MGR_TASK_NAME         "mgr-task"
+#define MGR_TASK_STACK_SIZE   4096
+#define MGR_TASK_PRIORITY     10
+
+
 static const char* TAG = "EWHC::MANAGER";
 
 
 static int mgr_modules_cnt = MGR_REG_LIST_CNT;
+
+static QueueHandle_t mgr_msg_queue = NULL;
+static TaskHandle_t mgr_TaskId = NULL;
+
 
 static esp_err_t mgr_Init(int id) {
   esp_err_t result = ESP_OK;
@@ -58,6 +70,17 @@ static esp_err_t mgr_Done(int id) {
   return result;
 }
 
+static void mgr_TaskFn(void* param) {
+  bool loop = true;
+
+  ESP_LOGI(TAG, "++%s()", __func__);
+  while (loop) {
+    ESP_LOGI(TAG, "[%s] Loop...", __func__);
+    vTaskDelay(pdMS_TO_TICKS(1000));
+  }
+  ESP_LOGI(TAG, "--%s()", __func__);
+}
+
 /**
  * @brief Init manager
  * 
@@ -67,7 +90,13 @@ esp_err_t MGR_Init(void) {
   esp_err_t result = ESP_OK;
 
   ESP_LOGI(TAG, "++%s()", __func__);
-  //mgr_modules_cnt = sizeof(mgr_reg_list)/sizeof(mgr_reg_t);
+
+  /* Initialization message queue */
+  mgr_msg_queue = xQueueCreate(MGR_MSG_MAX, sizeof(msg_t));
+ 
+  /* Initialization manager thread */
+  xTaskCreate(mgr_TaskFn, MGR_TASK_NAME, MGR_TASK_STACK_SIZE, NULL, MGR_TASK_PRIORITY, &mgr_TaskId);
+
   ESP_LOGI(TAG, "Modules to register: %d", mgr_modules_cnt);
   for (int idx = 0; idx < mgr_modules_cnt; ++idx) {
     result = mgr_Init(idx);
