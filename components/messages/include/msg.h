286f7344444d2c75b548a2eeaedfbbe13518fde2
Arkadiusz Czerwinski <A.Czerwinski@pistacje.net>
2025-01-31 20:53:25 -0600

    Add message definition

--- components/messages/include/msg.h
+++ components/messages/include/msg.h
@@ -15,4 +15,28 @@
 #include "esp_err.h"
 
 
+#define MSG_TOPIC_SIZE      20
+#define MSG_PAYLOAD_SIZE    100
+
+typedef char msg_topic_t[MSG_TOPIC_SIZE];
+typedef char msg_payload_t[MSG_PAYLOAD_SIZE];
+
+typedef enum {
+  MSG_TYPE_ETH,
+  MSG_TYPE_CLI,
+  MSG_TYPE_GPIO,
+  MSG_TYPE_POWER,
+  MSG_TYPE_MQTT,
+
+  MSG_TYPE_TEMPLATE,
+
+} msg_type_e;
+
+typedef struct {
+  msg_type_e    type;
+  msg_topic_t   topic;
+  msg_payload_t payload;
+} msg_t;
+
+
 #endif /* __MSG_H__ */
